637945708439e6918a98c22baa9f24dd9e2f5d2c
Ashish Jain <ashishjain99@yahoo.com>
2025-03-12 12:29:04 -0600

    Added Router

---
+++ cantva/src/lib/components/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+import {Router, Link, Route } from 'svelte-routing';
+
+import Home from '../../routes/Home.svelte';
+import About from '../../routes/About.svelte';
+</script>
+<Router>
+    <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+</nav>
+<div>
+    <Route path="/"> <Home/> </Route>
+    <Route path="/about/:id" let:params> <About id="{params.id}"/> </Route>
+    <Route path="/"> <Home /> </Route>
+</div>
+</Router>
---
+++ cantva/src/routes/About.svelte
@@ -0,0 +1,7 @@
+<script>
+const urlParams = new URLSearchParams(window.location.search);
+let param = $state(urlParams.get('id'))
+</script>
+<main>
+    <h1>This is about with id {param}</h1>
+</main>
